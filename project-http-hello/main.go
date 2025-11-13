8bf9e7beca801eb719d7eae614c9f717c201f2bb
aturzone <aturzone@gmail.com>
2025-11-13 15:25:59 +0330

    little project

--- project-http-hello/main.go
+++ project-http-hello/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
-	// Your solution goes here. Good luck!
+	http.HandleFunc("/hello", hello)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	fmt.Fprint(w, "Hello, ", name)
 }
